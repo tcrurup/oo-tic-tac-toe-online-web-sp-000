b5918fb804df5d9c0ad1f1e90d2a126e5e367c97
Anthony Rurup <tcrurup@gmail.com>
2019-04-30 21:09:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ class TicTacToe
   end
   
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index) ? true : false 
+    index.between?(0,8) && !self.position_taken?(index) ? true : false 
   end
   
   def turn_count
