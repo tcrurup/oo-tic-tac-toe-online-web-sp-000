5b17fe979ca49efcbfb7d42f63e68213afc84a6c
Anthony Rurup <tcrurup@gmail.com>
2019-04-30 21:09:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,7 +54,7 @@ class TicTacToe
   
   def turn 
     index = -1
-    until self.valid_move?(index)
+    until self.valid_move?(index) do
       puts "Enter a move from 1-9"
       input = gets.chomp
       index = self.input_to_index(input)
